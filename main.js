73b48d7df56cc496c909f801dee94d2423d14c43
Muntasir Tonmoy <muntasir01521566317@gmail.com>
2023-01-11 20:56:01 +0600

    current weather value updated from api

--- main.js
+++ main.js
@@ -18,4 +18,10 @@ function setValue(selector, value) {
 
 function renderCurrentWeather(current) {
   setValue("current-temp", current.currentTemp);
+  setValue("current-high", current.highTemp);
+  setValue("current-low", current.lowTemp);
+  setValue("current-fl-high", current.highFL);
+  setValue("current-fl-low", current.lowFL);
+  setValue("current-precip", current.windSpeed);
+  setValue("current-precip", current.precip);
 }
